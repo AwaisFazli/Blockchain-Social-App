827479bdccbd333c22fffebcefdffb45fe615586
arfha-ateeq <f20ba040@ibitpu.edu.pk>
2024-05-31 18:05:15 +0500

    Update static.js

--- client/lib/static.js
+++ client/lib/static.js
@@ -38,6 +38,17 @@ export const whoToFollow = [
     avatar:
       'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRMMDSV9N2qVWJwGQetckgZ0sJJxJ8f026cqjmB30DsOQ&s',
   },
+   {
+    name: 'Arfha Ateeq',
+    handle: '@arfha-ateeq',
+    avatar: 
+      'https://media.licdn.com/dms/image/D4D03AQEzrClMLOCsJw/profile-displayphoto-shrink_200_200/0/1702655581438?e=1722470400&v=beta&t=Ds3Ayi2fh1udw_b3Na1uH1ahWruab301nGgw-_CgRqs',
+  },
+  {
+    name: 'Wasi Abdullah',
+    handle: '@wasi-abdullah',
+    avatar: 'https://media.licdn.com/dms/image/D4D35AQEgVQ54yxl-Ug/profile-framedphoto-shrink_200_200/0/1694036928789?e=1717765200&v=beta&t=whuITprHN-QAzfnKefZajmyvEFHdlfqtEdUiFgYPoGc',
+  },
   {
     name: 'Sanity.io',
     handle: '@sanity_io',
